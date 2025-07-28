e434be5d8d8da1c66790ebb74d73ee0362bf39a1
Jack Mustonen <jack@cookietowncookies.com>
2025-07-28 09:28:39 -0700

    add lower case letter test

--- check_pwd.py
+++ check_pwd.py
@@ -1,4 +1,12 @@
 def check_pwd(password):
     if len(password) < 8 or len(password) > 20:
         return False
+
+    has_lowercase = False
+    for character in password:
+        if character >= 'a' and character <= 'z':
+            has_lowercase = True
+            break
+    if not has_lowercase:
+        return False
     return True
