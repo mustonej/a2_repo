75d56eb5b1da7561dc2352f31c4426270bff726b
Jack Mustonen <jack@cookietowncookies.com>
2025-07-28 09:23:33 -0700

    add test for short passwords

--- tests.py
+++ tests.py
@@ -5,6 +5,10 @@ class TestCheckPwd(unittest.TestCase):
     def test_empty_string_fails(self):
         result = check_pwd("")
         self.assertFalse(result)
+    
+    def test_short_password_fails(self):
+        result = check_pwd("Ab1!")
+        self.assertFalse(result)
 
 if __name__ == "__main__":
     unittest.main()
