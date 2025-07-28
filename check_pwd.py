0fa606891fbdccf454890dc88d2a5f7eee645df8
Jack Mustonen <jack@cookietowncookies.com>
2025-07-28 09:30:01 -0700

    add upper letter test

--- check_pwd.py
+++ check_pwd.py
@@ -3,10 +3,13 @@ def check_pwd(password):
         return False
 
     has_lowercase = False
+    has_uppercase = False
     for character in password:
         if character >= 'a' and character <= 'z':
             has_lowercase = True
-            break
-    if not has_lowercase:
+        if character >= 'A' and character <= 'Z':
+            has_uppercase = True
+
+    if not has_lowercase or not has_uppercase:
         return False
     return True
